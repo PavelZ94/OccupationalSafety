0fb8ec3df5b586d56801a680baa84cffb70a96cb
Павел <zhilinsky.pa@yandex.ru>
2024-10-06 01:00:49 +0300

    refactor user_handler, deleted old main.py

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -20,7 +20,7 @@ BOT_TOKEN=os.getenv('BOT_TOKEN')
 storage = MemoryStorage() #NEED TO REPLACE
 
 bot = Bot(BOT_TOKEN)
-dp = Dispatcher(storage=storage)
+#dp = Dispatcher(storage=storage)
 
 user_dict: dict[int, dict[str, str | int | bool]] = {}
 
@@ -36,46 +36,47 @@ class FSMFillForm(StatesGroup):
     #So, need to add  yagpt question may be and help
 
 
-@dp.message(CommandStart(), StateFilter(default_state))
+@router.message(CommandStart(), StateFilter(default_state))
 async def process_start_command(message: Message):
     await message.answer(
         text='Этот бот принимает заявки выявленных нарушений'
-            'требований охраны труда, промышленной и пожарной безопасности'
-            'Чтобы заявить о нарушении - отправьте команду - /mistake'
+            'требований охраны труда, промышленной и пожарной безопасности\n'
+            'Чтобы заявить о нарушении - отправьте команду - /mistake\n'
             'Чтобы задать вопрос нейросети в области охраны труда - '
-            'отправьте команду - /bot'
+            'отправьте команду - /bot\n'
             'Чтобы вызвать справку - отправьте команду - /help'
     )
 
-@dp.message(Command(commands='cancel'), StateFilter(default_state))
+@router.message(Command(commands='cancel'), StateFilter(default_state))
 async def process_cancel_command(message: Message):
     await message.answer(
-        text='Отменять нечего.'
+        text='Отменять нечего.\n'
              'Чтобы начать пользоваться ботом - '
              'отправьте команду - /mistake'
     )
 
-@dp.message(Command(commands='cancel'), ~StateFilter(default_state))
+@router.message(Command(commands='cancel'), ~StateFilter(default_state))
 async def process_cancel_command_state(message: Message, state: FSMContext):
     await message.answer(
-        text='Вы вышли из формы заполнения заявки нарушений'
+        text='Вы вышли из формы заполнения заявки нарушений.\n'
             'Чтобы снова перейти к заполнению заявки -'
             'отправьте команду /mistake'
     )
     await  state.clear()
 
-@dp.message(Command(commands='mistake'), StateFilter(default_state))
+@router.message(Command(commands='mistake'), StateFilter(default_state))
 async def process_mistake_command(message: Message, state: FSMContext):
     await message.answer(text=' Пожалуйста, введите ваше имя')
     await state.set_state(FSMFillForm.fill_name)
 
-@dp.message(StateFilter(FSMFillForm.fill_name), F.text.isalpha())
+@router.message(StateFilter(FSMFillForm.fill_name), F.text.isalpha())
 async def process_name_sent(message:Message, state: FSMContext):
     await state.update_data(name=message.text)
-    await message.answer(text='Благодарю!\n\n Теперь укажите краткую информацию о нарушении')
+    await message.answer(text='Благодарю!\n\n'
+                              'Теперь укажите краткую информацию о нарушении')
     await state.set_state(FSMFillForm.fill_mistake)
 
-@dp.message(StateFilter(FSMFillForm.fill_name))
+@router.message(StateFilter(FSMFillForm.fill_name))
 async def warning_not_name(message: Message):
     await message.answer(
         text='То, что вы отправили не похоже на имя\n\n'
@@ -84,15 +85,16 @@ async def warning_not_name(message: Message):
              'отправьте команду /cancel'
     )
 
-@dp.message(StateFilter(FSMFillForm.fill_mistake))
+@router.message(StateFilter(FSMFillForm.fill_mistake))
 async def process_mistake_sent(message: Message, state: FSMContext):
     await state.update_data(mistake=message.text)
-    await message.answer(text='Благодарю!\n\n Теперь укажите подробную информацию о нарушении')
+    await message.answer(text='Благодарю!\n\n'
+                              'Теперь укажите подробную информацию о нарушении')
     await state.set_state(FSMFillForm.fill_description)
 
 #NEED TO ADD HANDLER, CATCHING MISTAKE WITH MISTAKE ERROR
 
-@dp.message(StateFilter(FSMFillForm.fill_description))
+@router.message(StateFilter(FSMFillForm.fill_description))
 async def process_description_sent(message: Message, state: FSMContext):
     await state.update_data(description=message.text)
     low_level_button = InlineKeyboardButton(
@@ -111,13 +113,15 @@ async def process_description_sent(message: Message, state: FSMContext):
         [low_level_button, medium_level_button, high_level_button]
     ]
     markup = InlineKeyboardMarkup(inline_keyboard=keyboard)
-    await message.answer(text='Благодарю!\n\n Теперь укажите предполагаемый уровень опасности',
+    await message.answer(text='Благодарю!\n\n'
+                              'Теперь укажите предполагаемый уровень опасности',
                          reply_markup=markup)
     await state.set_state(FSMFillForm.fill_level)
 
 
-@dp.message(StateFilter(FSMFillForm.fill_level),
-            F.data.in_(['low', 'medium', 'high']))
+@router.callback_query(F.data.in_(['low', 'medium', 'high']))
+#message(StateFilter(FSMFillForm.fill_level),
+#            F.data.in_(['low', 'medium', 'high']))
 async def process_level_press(callback: CallbackQuery, state: FSMContext):
     await state.update_data(level=callback.data)
     await callback.message.delete()
@@ -126,33 +130,39 @@ async def process_level_press(callback: CallbackQuery, state: FSMContext):
     )
     await state.set_state(FSMFillForm.fill_place)
 
-@dp.message(StateFilter(FSMFillForm.fill_level))
+@router.message(StateFilter(FSMFillForm.fill_level))
 async def warning_not_level(message: Message):
     await message.answer(
         text='Пожалуйста, пользуйтесь кнопками '
-             'при выборе уровня опасности\n\nЕсли вы хотите прервать '
+             'при выборе уровня опасности\n\n'
+             'Если вы хотите прервать '
              'заполнение анкеты - отправьте команду /cancel'
     )
 
-@dp.message(StateFilter(FSMFillForm.fill_place))
+@router.message(StateFilter(FSMFillForm.fill_place))
 async def process_place_sent(message: Message, state: FSMContext):
     await state.update_data(place=message.text)
-    await message.answer(text='Благодарю!\n\n Теперь загрузите фотографию нарушения')
+    await message.answer(text='Благодарю!\n\n'
+                              'Теперь загрузите фотографию нарушения')
     await state.set_state(FSMFillForm.upload_photo)
 
-@dp.message(StateFilter(FSMFillForm.upload_photo),
+@router.message(StateFilter(FSMFillForm.upload_photo),
             F.photo[-1].as_('latest_photo'))
 async def process_photo_sent(message: Message,
                              state: FSMContext,
                              latest_photo: PhotoSize):
+    user_id = message.from_user.id
+
     await state.update_data(
         photo_unique_id=latest_photo.file_unique_id,
         photo_id=latest_photo.file_id
     )
-    user_dict[latest_photo.from_user.id] = await state.get_data()
+
+    user_dict[user_id] = await state.get_data()
+    #user_dict[latest_photo.from_user.id] = await state.get_data()
     await state.clear()
     await message.answer(
-        text='Благодарю! Ваша заявка зарегистрирована.'
+        text='Благодарю! Ваша заявка зарегистрирована.\n'
         'Сотрудники Отдела охраны труда обработают его в '
         'установленные сроки и сообщат о результатах рассмотрения'
     )
